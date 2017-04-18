97a5e98f2aa980e18d6992d9a61c4907bd3efc25
shenn13 <shenn13@163.com>
2017-04-18 16:21:19 +0800

    检测更新完善

--- MyVideo/MyVideo/MovieVideo/MySetting/Controllers/MySettingViewController.m
+++ MyVideo/MyVideo/MovieVideo/MySetting/Controllers/MySettingViewController.m
@@ -34,7 +34,6 @@
     
     UITableView *_tableView;
 }
-@property (nonatomic , strong)NSDictionary *updateDic;
 @property(nonatomic,strong) GGActionSheet *actionSheetTitle;
 
 @end
@@ -202,8 +201,8 @@
     
     XBSettingItemModel *secItem8 = [[XBSettingItemModel alloc] init];
     secItem8.funcName = @"检测更新";
-    secItem8.img = [UIImage imageNamed:@""];
-    secItem8.detailText = @"已是最新版";
+//    secItem8.img = [UIImage imageNamed:@""];
+//    secItem8.detailText = @"已是最新版";
     secItem8.executeCode = ^{
         
         [self update];
@@ -481,23 +480,48 @@
  *******/
 -(void)update{
     
+    NSString *newVersion;
+    NSURL *url = [NSURL URLWithString:@"http://itunes.apple.com/cn/lookup?id=1219112934"];
+    //这个URL地址是该app在iTunes connect里面的相关配置信息。其中id是该app在app store唯一的ID编号。
     
-    double localVersion =[[[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"] doubleValue];
+    NSString *jsonResponseString = [NSString stringWithContentsOfURL:url encoding:NSUTF8StringEncoding error:nil];
     
-    if (localVersion < (double)[[self.updateDic objectForKey:@"versionCode"] doubleValue]) {
-        
-        UIAlertView *alertV = [[UIAlertView alloc] initWithTitle:@"发现新版本" message:[self.updateDic objectForKey:@"releaseNote"] delegate:self cancelButtonTitle:nil otherButtonTitles:@"确定", nil];
-        [self.view addSubview:alertV];
-        
-        [alertV show];
-        return;
+    NSLog(@"通过appStore获取的数据信息：%@",jsonResponseString);
+    
+    NSData *data = [jsonResponseString dataUsingEncoding:NSUTF8StringEncoding];
+    id json = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+    NSArray *array = json[@"results"];
+    for (NSDictionary *dic in array) {
+        newVersion = [dic valueForKey:@"version"];
     }
+    NSLog(@"通过appStore获取的版本号是：%@",newVersion);
+    //获取本地软件的版本号
+    NSString *localVersion = [[[NSBundle mainBundle]infoDictionary] objectForKey:@"CFBundleVersion"];
+    
+    NSString *msg = [NSString stringWithFormat:@"你当前的版本是V%@，发现新版本V%@，是否下载新版本？",localVersion,newVersion];
+    
+    //对比发现的新版本和本地的版本
+    if ([newVersion floatValue] > [localVersion floatValue]){
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"升级提示"message:msg preferredStyle:UIAlertControllerStyleAlert];
+        
+        [self presentViewController:alert animated:YES completion:nil];
+        
+        [alert addAction:[UIAlertAction actionWithTitle:@"现在升级" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+            
+            [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://itunes.apple.com/us/app/%E6%96%97%E5%9B%BE%E8%A1%A8%E6%83%85%E5%8C%85%E5%A4%A7%E5%85%A8-%E5%8A%A8%E6%80%81gif%E7%BE%8E%E5%9B%BE%E5%B7%A5%E5%8E%82/id1219112934?l=zh&ls=1&mt=8"]];
+            
+            //            这里写的URL地址是该app在app store里面的下载链接地址，其中ID是该app在app store对应的唯一的ID编号。
+            NSLog(@"点击现在升级按钮");
+        }]];
+        
+        [alert addAction:[UIAlertAction actionWithTitle:@"下次再说" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+            NSLog(@"点击下次再说按钮");
+        }]];
+    }
+
     
 }
-- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
-    NSURL *url = [NSURL URLWithString:[self.updateDic objectForKey:@"appUrl"]];
-    [[UIApplication sharedApplication] openURL:url];
-}
+
 
 
 
